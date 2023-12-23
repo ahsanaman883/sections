c95833a54bb0e0f6c0360853af105c86f4f198f1
ahsanaman123 <ahsanaman123@gmail.com>
2023-12-23 13:57:11 +0500

    add hero

--- sections/src/app/app.module.ts
+++ sections/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { Hero1Component } from './hero1/hero1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    Hero1Component
   ],
   imports: [
     BrowserModule,
